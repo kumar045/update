e8c1e1570f2155e09f5db77e17063879cd54b840
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-02-16 23:02:39 +0200

    Play sound using kivy.core.SoundLoader

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 __version__ = '1.0.0'
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from playsound import playsound
+from kivy.core.audio import SoundLoader
 
 
 class HelloCodi(MDApp):
     def build(self):
         return Builder.load_file('assets/main.kv')
     def buzz(self, *args):
-        playsound('assets/Bee-Sound.mp3')
-
+        sound = SoundLoader.load('assets/Bee-Sound.mp3')
+        if sound:
+            sound.play()
 
 HelloCodi().run()        
